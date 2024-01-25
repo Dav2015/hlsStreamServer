1d513e67f013393bf59223891287308a53e97a9e
David Câmara <d.camara@tuta.io>
2024-01-25 17:43:13 +0000

    all

---
+++ index.js
@@ -0,0 +1,30 @@
+const HLSServer = require('hls-server');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HLS server configuration
+const server = new HLSServer(app, {
+    path: '/stream', // The endpoint for serving HLS streams
+    dir: 'public/videos', // Directory where your video files are stored
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.get('/stream/:file', (req, res) => {
+    const file = req.params.file;
+    console.log(`Request for variant: ${file}`);
+
+    const filePath = path.resolve(__dirname, 'public/videos', file);
+    res.sendFile(filePath);
+});
+
+
---
+++ m3u8generator.js
@@ -0,0 +1,36 @@
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+
+const inputVideo = './input.mp4';
+const outputFolder = 'public/videos';
+
+const variants = [
+    { resolution: '640x360', bitrate: '1100000' },
+    { resolution: '852x480', bitrate: '1460000' },
+    { resolution: '1280x720', bitrate: '3290000' },
+    { resolution: '1920x1080', bitrate: '6610000' },
+];
+
+variants.forEach((variant, index) => {
+    const outputName = `variant-${index + 1}`;
+    const outputM3U8 = path.join(outputFolder, `${outputName}.m3u8`);
+
+    ffmpeg(inputVideo)
+        .outputOptions([
+            '-c:v h264',
+            '-c:a aac',
+            '-strict -2',
+            '-f hls',
+            '-hls_time 6', // Set the segment duration (in seconds)
+            '-hls_list_size 0', // Set to 0 for an infinite playlist
+            `-b:v ${variant.bitrate}`,
+            `-s ${variant.resolution}`,
+        ])
+        .on('end', () => {
+            console.log(`Conversion for ${outputName} finished!`);
+        })
+        .on('error', (err) => {
+            console.error(`Error for ${outputName}:`, err);
+        })
+        .save(outputM3U8);
+});
